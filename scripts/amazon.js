4a17edfece4d7eee492137ad998237f11f14d537
mahes <maheswaran2527@gmail.com>
2025-02-11 14:14:33 +0530

    use moduleand import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,3 @@
-const  products =[
-    {
-   image: 'images/products/athletic-cotton-socks-6-pairs.jpg ',
-   name:   'Black and Gray Athletic Cotton Socks - 6 Pairs',
-   rating:{
-    star:4.5,
-    count:87
-   },
-   price: 1090
-
-    },{
-        image:'images/products/intermediate-composite-basketball.jpg',
-        name:   'Intermediate Size Basketball',
-        rating:{
-            star:4,
-            count:127
-           },
-           price: 2095
-
-    },{
-        image:' images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-         name:   ' Adults Plain Cotton T-Shirt - 2 Pack',
-
-        rating:{
-            star:4.5,
-            count:56
-           },
-           price: 799
-
-
-    }
-];
 
 let productHTML='';
 
@@ -46,7 +14,7 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-            src="images/ratings/rating-${product.rating.star *10}.png">
+            src="images/ratings/rating-${product.rating.stars *10}.png">
 
             <div class="product-rating-count link-primary">
               ${product.rating.count}
@@ -54,7 +22,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.price/100).toFixed(2)}
+            $${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -79,12 +47,50 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
-            Add to Cart
+          <button class="add-to-cart-button button-primary js-add-to-cart"data-product-id="${product.id}">
+            Add to Cart 
           </button>
         </div>`;
 
 });
 
-console.log(productHTML);
 document.querySelector('.js-product-grid').innerHTML=productHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+   button.addEventListener('click',()=>{
+    const productId=button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+        if(productId===item.productId){
+            matchingItem=item;
+
+        }
+        
+    });
+
+    if (matchingItem){
+        matchingItem=quantity+=1;
+    }else{
+        cart.push({
+            productId:productId,
+            quantity:1
+    
+        });
+    }
+    let carQuality=0;
+    cart.forEach((item)=>{
+     
+        carQuality+=item.quantity;
+
+    }
+    ); 
+document.querySelector('.js-cart-quantity')
+.innerHTML=carQuality;
+
+
+});
+ 
+});
